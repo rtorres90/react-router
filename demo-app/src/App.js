dfd19fd3e34c2c80a55ec7a41a5f2ddf7882a720
Roberto Torres <roberto.torres.dev@gmail.com>
2018-08-26 08:33:56 -0700

    my first routes

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 
 class App extends Component {
   render() {
     return (
       <Router>
-
+        <div>
+          <Route path='/' exact strict render={
+            () => {
+              return (<h1>Welcome home!</h1>)
+            }
+          } />
+          <Route path='/about/' exact strict render={
+            () => {
+              return (<h1>Welcome about!</h1>)
+            }
+          } />
+        </div>
       </Router>
     );
   }
