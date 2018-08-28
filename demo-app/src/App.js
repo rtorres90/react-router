327f97e36ee6c63c02c0db080b4eca837b3362a2
Roberto Torres <roberto.torres.dev@gmail.com>
2018-08-27 19:17:01 -0700

    links

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import Route from 'react-router-dom/Route';
+
+const User = ({match}) => {
+  return (<h1>Welcome User! {match.params.username}</h1>);
+}
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/user/Roberto">Roberto</Link>
+            </li>
+            <li>
+              <Link to="/user/Minerva">Minerva</Link>
+            </li>
+          </ul>
           <Route path='/' exact strict render={
             () => {
               return (<h1>Welcome home!</h1>)
@@ -18,6 +36,7 @@ class App extends Component {
               return (<h1>Welcome about!</h1>)
             }
           } />
+          <Route path='/user/:username' exact strict component={User} />
         </div>
       </Router>
     );
