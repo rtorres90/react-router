c9c1d6aae6faa56d750dae20b0ed565a607955b8
Roberto Torres <roberto.torres.dev@gmail.com>
2018-08-31 12:55:52 -0700

    States.

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Link , NavLink} from 'react-router-dom';
+import { BrowserRouter as Router, Link, NavLink, Redirect } from 'react-router-dom';
 import Route from 'react-router-dom/Route';
 
-const User = ({ match }) => {
-  return (<h1>Welcome User! {match.params.username}</h1>);
+const User = (params) => {
+  return (<h1>Welcome User! {params.username}</h1>);
 }
 
 class App extends Component {
+  state = {
+    loggedIn: false
+  };
+  loginHangle = () => {
+    this.setState(prevState => (
+      { loggedIn: !prevState.loggedIn }
+    ));
+  }
   render() {
     return (
       <Router>
@@ -42,6 +50,7 @@ class App extends Component {
               }>Minerva</NavLink>
             </li>
           </ul>
+          <input type="button" value={this.state.loggedIn ? 'Log Out': 'Log In'} onClick={this.loginHangle.bind(this)} />
           <Route path='/' exact strict render={
             () => {
               return (<h1>Welcome home!</h1>)
@@ -52,7 +61,11 @@ class App extends Component {
               return (<h1>Welcome about!</h1>)
             }
           } />
-          <Route path='/user/:username' exact strict component={User} />
+          <Route path='/user/:username' exact strict render={
+            ({ match }) => (
+              this.state.loggedIn ? (<User username={match.params.username} />) : (<Redirect to='/' />)
+            )
+          } />
         </div>
       </Router>
     );
